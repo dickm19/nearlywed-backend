f8414ccfb8a9dcf9a608820c1bd4f4f44ab82458
Mary Dick <maryclairedick@gmail.com>
2025-11-06 12:44:36 -0600

    add guest model

---
+++ app/models/guest.rb
@@ -0,0 +1,9 @@
+class Guest < ApplicationRecord
+    belongs_to :wedding
+
+    validates :email, uniqueness: { case_sensitive: false }
+    validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    validates_presence_of :first_name
+    validates_presence_of :phone_number
+
+end
