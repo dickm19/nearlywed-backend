309159628d908bffb74a338ad1b2e2d8039e0363
Mary Dick <maryclairedick@gmail.com>
2025-11-06 12:59:05 -0600

    remove guest_invite mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,12 +4,4 @@ class UserMailer < ApplicationMailer
         @url = "http://localhost:3000"
         mail(to: @user.email, subject: "Welcome to NearlyWed!")
     end
-
-    def guest_invite
-        @user = params[:user]
-        @wedding = params[:wedding]
-        @recipient_emails = params[:guest_emails]
-        @url = "http://localhost:3000/guest_signup"
-        mail(to: @recipient_emails, subject: "Save the Date for #{@wedding.date.strftime("%B %d, %Y")}!")
-    end
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -16,20 +16,4 @@ RSpec.describe UserMailer, type: :mailer do
       expect(mail.body.encoded).to match("Thanks for joining and have a great day!")
     end
   end
-
-  describe "guest_invite" do
-    let(:wedding) { Wedding.create(couple_names: "John and Jane", date: "2023-12-31") }
-    let(:guest_emails) { [ "guest1@example.com", "guest2@example.com" ] }
-    let(:mail) { UserMailer.with(wedding: wedding, guest_emails: guest_emails).guest_invite }
-
-    it "renders the headers" do
-      expect(mail.subject).to eq("Save the Date for December 31, 2023!")
-      expect(mail.to).to eq(guest_emails)
-      expect(mail.from).to eq([ "nearlywed1@gmail.com" ])
-    end
-
-    it "renders the body" do
-      expect(mail.body.encoded).to match("You've been Invited to John and Jane's wedding on December 31, 2023")
-    end
-  end
 end
