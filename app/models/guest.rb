01f8cf9d8c4040df816107fdfe26d1166faa7715
Mary Dick <maryclairedick@gmail.com>
2025-11-06 13:46:14 -0600

    only validate email format and uniqueness if present

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,8 +1,14 @@
 class Guest < ApplicationRecord
     belongs_to :wedding
 
-    validates :email, uniqueness: { case_sensitive: false }
-    validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    validates :email, uniqueness: { case_sensitive: false }, if: :email_present?
+    validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, if: :email_present?
     validates_presence_of :first_name
     validates_presence_of :phone_number
+
+    private
+
+    def email_present?
+      email.present?
+    end   
 end
